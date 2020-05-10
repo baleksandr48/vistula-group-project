33d8ea3933b895cf3674694d13c99a8c1f3a6440
birukov.aleksandr48 <birukov.aleksandr48@gmail.com>
2020-05-10 17:32:59 +0200

    add database.sql

---
+++ database.sql
@@ -0,0 +1,33 @@
+create table "Category"
+(
+    id          serial       not null
+        constraint "Category_pkey"
+            primary key,
+    name        varchar(255) not null,
+    description varchar(255) not null
+);
+
+create table "Book"
+(
+    id           serial       not null
+        constraint "Book_pkey"
+            primary key,
+    title        varchar(255) not null,
+    description  varchar(255) not null,
+    "categoryId" integer      not null
+        constraint "Book_categoryId_fkey"
+            references "Category"
+            on delete cascade
+);
+
+create table "Opinion"
+(
+    id       serial       not null
+        constraint "Opinion_pkey"
+            primary key,
+    content  varchar(255) not null,
+    "bookId" integer      not null
+        constraint "Opinion_bookId_fkey"
+            references "Book"
+            on delete cascade
+);
